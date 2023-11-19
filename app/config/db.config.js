e37ba4306f60eb6dd0af60b7ba49320be2dc0aab
YEYE <63726895+Thasada@users.noreply.github.com>
2023-11-20 05:56:41 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "sql.freedb.tech",
-  USER: "freedb_Nititorn_HW5",
-  PASSWORD: "?7eVScvAgMyVQWX",
-  DB: "freedb_Nititorn_HW5",
+  USER: "freedb_thass",
+  PASSWORD: "q9yDssw#!%vjvJ8",
+  DB: "freedb_web_programming",
 };
 
